435721966949162bb4d3d40eba9489914d35232f
Mark Benewith <mark@benewith.com>
2019-01-30 12:22:21 +0000

    xinc login implementation and styling

---
+++ template-xinc-login.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Xinc Login
+ *
+ * @package WordPress
+ */
+?>
+
+<?php get_header(); ?>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-12 col-sm-8 xinc-login">
+      <?php while (have_posts()) : the_post(); ?>
+        <h1><?php echo \Tofino\Helpers\title(); ?></h1>
+        <?php the_content(); ?>
+      <?php endwhile; ?>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
