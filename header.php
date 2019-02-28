aa18f1f2dd2c3cf161612de76e6f0cb4f783e98e
Mark Benewith <mark@benewith.com>
2019-02-28 13:03:49 +0000

    added GA

--- header.php
+++ header.php
@@ -4,6 +4,16 @@ use \Tofino\ThemeOptions\Notifications as n; ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-75130451-4"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-75130451-4');
+  </script>
+
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
