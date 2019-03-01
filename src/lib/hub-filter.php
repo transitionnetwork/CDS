d1a290ba3be8883b2457a313dbee40a3c31d1b2a
Mark Benewith <mark@benewith.com>
2019-03-01 10:36:56 +0000

    fix default initiative value and top bar

--- src/lib/hub-filter.php
+++ src/lib/hub-filter.php
@@ -1,28 +1,4 @@
 <?php
-// function get_hub_terms() {
-//   $term_slugs = wp_cache_get('term_slugs');
-//   if (false === $term_slugs) {
-//     $args = array(
-//       'post_type' => 'initiatives',
-//       'post_status' => 'publish',
-//       'posts_per_page' => -1
-//     );
-//     $posts = get_posts($args);
-//     $term_ids = array();
-//     foreach ($posts as $post) {
-//       $term_ids[] = get_field('hub_tax', $post->ID);
-//     }
-//     $term_ids = array_unique($term_ids);
-//     foreach ($term_ids as $term_id) {
-//       $term_slugs[] = get_term_by('id', $term_id, 'hub')->slug;
-//     }
-//     sort($term_slugs);
-//     wp_cache_set('term_slugs', $term_slugs);
-//   }
-
-//   return $term_slugs;
-// }
-
 function render_hub_filter() {
   //TODO: Cache this filter generation script. Also add counts?
   $terms = get_terms('hub'); ?>
--- templates/logged-in-as.php
+++ templates/logged-in-as.php
@@ -1,18 +1,25 @@
 <?php
 $user_role = wp_get_current_user()->roles[0];
 $user_human_role = ucwords(str_replace('_', ' ', $user_role));
-$user_hub = get_the_terms(wp_get_current_user(), 'hub')[0]->name;
+$user_hub = get_the_terms(wp_get_current_user(), 'hub')[0]->name; ?>
 
-if(is_user_logged_in()) : ?>
-  <?php _e('Logged in as', 'tofino'); ?> <?php echo wp_get_current_user()->user_login; ?>
-<?php else : ?>
-  <?php _e('Not logged in', 'tofino'); ?> | <a href="<?php echo home_url('member-login'); ?>"><?php _e('Login', 'tofino'); ?></a>
-<?php endif; ?>
+<div class="user-details">
+  <?php
+  if(is_user_logged_in()) : ?>
+    <?php _e('Logged in as', 'tofino'); ?> <?php echo wp_get_current_user()->user_login; ?>
+  <?php else : ?>
+    <?php _e('Not logged in', 'tofino'); ?> | <a href="<?php echo get_the_permalink(459); ?>"><?php _e('Login', 'tofino'); ?></a>
+  <?php endif; ?>
 
-<?php if($user_role) : ?>
-  <div class="tag role"><?php _e('Role', 'tofino'); ?>: <?php echo $user_human_role; ?></div>
-<?php endif; ?>
+  <?php if($user_role) : ?>
+    <div class="tag role"><?php _e('Role', 'tofino'); ?>: <?php echo $user_human_role; ?></div>
+  <?php endif; ?>
 
-<?php if($user_hub) : ?>
-  <div class="tag hub"><?php echo $user_hub; ?></div>
-<?php endif; ?>
+  <?php if($user_hub) : ?>
+    <div class="tag hub"><?php echo $user_hub; ?></div>
+  <?php endif; ?>
+</div>
+
+<div>
+  <span class="tag beta">BETA</span>
+</div>
