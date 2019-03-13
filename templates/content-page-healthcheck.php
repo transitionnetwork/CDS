fe81853f34fe1f73e25c585b917efc0cef0333db
Mark Benewith <mark@benewith.com>
2019-03-13 12:26:59 +0000

    change col width

--- templates/content-page-healthcheck.php
+++ templates/content-page-healthcheck.php
@@ -1,7 +1,7 @@
 <main>
   <div class="container">
     <div class="row">
-      <div class="col-12">
+      <div class="col-12 col-lg-8">
         <?php while (have_posts()) : the_post(); ?>
           <h1><?php echo \Tofino\Helpers\title(); ?></h1>
           <p><a href="<?php echo get_field('healthcheck_pdf') ?>" onclick="ga('send', 'event', 'PDF Downloads', 'Download', '<?php echo ICL_LANGUAGE_CODE; ?>', '0');">
