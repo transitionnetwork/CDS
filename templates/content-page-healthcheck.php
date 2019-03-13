5bcb683b86dd98084c806150f42a7d44668d6f5d
Mark Benewith <mark@benewith.com>
2019-03-13 12:22:06 +0000

    added GA tracking to PDF downloads

--- templates/content-page-healthcheck.php
+++ templates/content-page-healthcheck.php
@@ -4,7 +4,7 @@
       <div class="col-12">
         <?php while (have_posts()) : the_post(); ?>
           <h1><?php echo \Tofino\Helpers\title(); ?></h1>
-          <p><a href="<?php echo get_field('healthcheck_pdf') ?>" download>
+          <p><a href="<?php echo get_field('healthcheck_pdf') ?>" onclick="ga('send', 'event', 'PDF Downloads', 'Download', '<?php echo ICL_LANGUAGE_CODE; ?>', '0');">
             <img src="<?php echo get_field('healthcheck_image')['url']; ?>" alt="<?php echo get_the_title(); ?>">
           </a></p>
           <?php the_content(); ?>
