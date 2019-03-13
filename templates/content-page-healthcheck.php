d7de8c46b6490bfa5dcc1d6360817604ce307b16
Mark Benewith <mark@benewith.com>
2019-03-13 12:16:51 +0000

    changed healthcheck download widget display method

---
+++ templates/content-page-healthcheck.php
@@ -0,0 +1,15 @@
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <?php while (have_posts()) : the_post(); ?>
+          <h1><?php echo \Tofino\Helpers\title(); ?></h1>
+          <p><a href="<?php echo get_field('healthcheck_pdf') ?>" download>
+            <img src="<?php echo get_field('healthcheck_image')['url']; ?>" alt="<?php echo get_the_title(); ?>">
+          </a></p>
+          <?php the_content(); ?>
+        <?php endwhile; ?>
+      </div>
+    </div>
+  </div>
+</main>
