f59d1cdd9ff8c5eefaae8b10048d34d48a161bff
Mark Benewith <mark@benewith.com>
2019-03-15 12:45:57 +0000

    migrated onclick to gtag

--- templates/content-page-healthcheck.php
+++ templates/content-page-healthcheck.php
@@ -4,7 +4,7 @@
       <div class="col-12 col-lg-8">
         <?php while (have_posts()) : the_post(); ?>
           <h1><?php echo \Tofino\Helpers\title(); ?></h1>
-          <p><a href="<?php echo get_field('healthcheck_pdf') ?>" onclick="ga('send', 'event', 'PDF Downloads', 'Download', '<?php echo ICL_LANGUAGE_CODE; ?>', '0');">
+          <p><a href="<?php echo get_field('healthcheck_pdf') ?>" onclick="gtag('event', 'PDF Download', {'event_category' : 'downloads', 'event_label' : '<?php echo ICL_LANGUAGE_CODE; ?>' });">
             <img src="<?php echo get_field('healthcheck_image')['url']; ?>" alt="<?php echo get_the_title(); ?>">
           </a></p>
           <?php the_content(); ?>
