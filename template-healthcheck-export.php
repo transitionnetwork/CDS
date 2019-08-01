d282b7e0e9514670b1bf54b9196f95dbb9a7ed6b
Mark Benewith <mark@benewith.com>
2019-08-01 17:24:38 +0100

    improve healthcheck export

--- template-healthcheck-export.php
+++ template-healthcheck-export.php
@@ -21,14 +21,21 @@ foreach($posts as $post) {
   if(!in_array($post->post_title, $healthcheck_ids)) {
     $healthcheck_ids[] = $post->post_title;
     $question_groups = get_fields($post->ID);
-    foreach($question_groups as $key => $group) {
-      foreach ($group as $label => $response) {
-        // add question the first time
-        if($i == 0) {
-          $full_question = get_field_object($key . '_' . $label)['label'];
-          $questions[] = $full_question;
+    
+    $initiative_title = get_post($post->post_title);
+    
+    if($question_groups) {
+      $questions[0] = 'Hub';
+      $answers[$i][0] = $initiative_title->post_title;
+      foreach($question_groups as $key => $group) {
+        foreach ($group as $label => $response) {
+          // add question the first time
+          if($i == 0) {
+            $full_question = get_field_object($key . '_' . $label)['label'];
+            $questions[] = $full_question;
+          }
+          $answers[$i][] = $response;
         }
-        $answers[$i][] = $response;
       }
     }
   }
@@ -40,4 +47,5 @@ foreach($answers as $answer_set) {
   $export_data[] = $answer_set;
 }
 
+// var_dump($export_data);
 outputCsv(date('Ymd') . '_healthcheck_responses.csv', $export_data); ?>
