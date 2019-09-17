69aa93cdf8fbe7b23523a44a0c899b88d0b68b49
Mark Benewith <mark@benewith.com>
2019-09-17 13:41:32 +0100

    add healthcheck updated message

--- templates/content-page-add-healthcheck.php
+++ templates/content-page-add-healthcheck.php
@@ -22,7 +22,7 @@ if (!is_user_logged_in() || (!can_view_healthcheck($post))) {
               'post_id'		=> 'new_post',
               'post_title'	=> false,
               'post_content'	=> false,
-              'return' => get_permalink($initiative_id),
+              'return' => add_query_arg('updated', 'healthcheck', get_permalink($initiative_id)),
               'submit_value' => 'Create Healthcheck',
               'new_post'		=> array(
                 'post_type'		=> 'healthchecks',
