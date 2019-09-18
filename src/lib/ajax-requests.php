e43dd2a09ec71fa4ddb4e9c7cbcda4068d4d25bf
Mark Benewith <mark@benewith.com>
2019-09-18 16:53:23 +0100

    fix hub integration

--- src/lib/ajax-requests.php
+++ src/lib/ajax-requests.php
@@ -35,15 +35,18 @@ function return_map_markers() {
 
   $posts = get_posts($args);
 
+
   if ( false === ( $i_markers = get_transient($hash))) {
-    foreach($posts as $key => $post) {
-      $map = get_field('map', $post->ID, false);
+    $key = 0;
+    foreach($posts as $post) {
+      $map = get_field('map', $post->ID);
       if(!empty($map['markers'])) {
         $i_markers[$key]['lat'] = $map['lat'];
         $i_markers[$key]['lng'] = $map['lng'];
         $i_markers[$key]['permalink'] = get_the_permalink($post->ID);
         $i_markers[$key]['title'] = get_the_title($post->ID);
         // $markers[$key]['excerpt'] = get_the_excerpt($post->ID);
+        $key++;
       }
     }
 	// Put the results in a transient. Expire after 12 hours.
@@ -54,9 +57,14 @@ function return_map_markers() {
   $hubs = get_terms('hub', array(
     'hide_empty' => false
   ));
-  $key = 0;
 
+  if($_POST['value']['hub_name']) {
+    $hubs = []; //must be array
+    $hubs[] = get_term_by('slug', $_POST['value']['hub_name'], 'hub');
+  }
+  
   //TODO add hub transient in here
+  $key = 0;
   foreach($hubs as $hub) {
     $map = get_field('map', $hub);
     if(!empty($map['markers'])) {
@@ -68,8 +76,13 @@ function return_map_markers() {
     }
   }
 
-  $data['i_markers'] = $i_markers;
-  $data['h_markers'] = $h_markers;
+  if($i_markers) {
+    $data['i_markers'] = $i_markers;
+  }
+
+  if($h_markers) {
+    $data['h_markers'] = $h_markers;
+  }
 
   echo json_encode($data);
   wp_die();
