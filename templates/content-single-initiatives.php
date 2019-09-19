8baf608e31383326730f101eae458020de5eb120
Mark Benewith <mark@benewith.com>
2019-09-19 14:44:28 +0100

    fix single initiative

--- templates/content-single-initiatives.php
+++ templates/content-single-initiatives.php
@@ -76,6 +76,8 @@
             <?php $map = get_field('map'); ?>
             <?php if($map) { ?>
               <div id="initiative-map" data-lat="<?php echo $map['lat']; ?>" data-lng="<?php echo $map['lng']; ?>" data-zoom="<?php echo $map['zoom']; ?>"></div>
+            <?php } ?>
+            
             <?php if (get_field('address_line_1')) { ?>
               <label><?php _e('Location', 'tofino'); ?></label>
               <?php echo get_field('address_line_1'); ?><br/>
