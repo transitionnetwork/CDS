bfd6638fcc0281c1c85aa3c14cfc262126464a8a
Mark Benewith <mark@benewith.com>
2019-12-19 12:05:28 +0000

    fix pending results not showing for hubs

--- templates/panels/hub-initiatives-pending-approval.php
+++ templates/panels/hub-initiatives-pending-approval.php
@@ -11,7 +11,7 @@
         'post_type' => 'initiatives',
         'posts_per_page' => -1,
         'post_status' => 'pending',
-        'field' => 'ids',
+        'fields' => 'ids',
         'tax_query' => array(
           array(
             'taxonomy' => 'hub',
@@ -21,8 +21,8 @@
         )
       );
       $post_ids = get_posts($args);
-    if ($posts_ids) :
-      list_initiatives($posts_ids);
+    if ($post_ids) :
+      list_initiatives($post_ids);
     else : ?>
       <?php _e('There aren\'t any initiatives pending approval for', 'tofino'); ?> <?php echo $term->name; ?>.
     <?php endif; ?>
