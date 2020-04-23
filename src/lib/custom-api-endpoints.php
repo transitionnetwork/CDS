9946fa31c0f73d33519550cb887b2c84bd1caf4a
Mark Benewith <mark@benewith.com>
2020-04-23 13:52:25 +0100

    added all data to api endpoint

--- src/lib/custom-api-endpoints.php
+++ src/lib/custom-api-endpoints.php
@@ -3,25 +3,68 @@
 function custom_initiative_endpoint() {
   $args = array(
     'post_type' => 'initiatives',
-    'posts_per_page' => -1
+    'posts_per_page' => 10
   );
 
   $posts = get_posts($args);
   $data = [];
+  $topic_output = '';
+  $hub_output = '';
 
   foreach($posts as $post) {
+    $topics = get_the_terms($post, 'topic');
+    if($topics) {
+      $topic_list = array();
+      foreach($topics as $topic) {
+        $topic_list[] = $topic->name;
+      }
+      
+      $topic_output = implode(' | ', $topic_list);
+    }
+
+    $hubs = get_the_terms($post, 'hub');
+    if($hubs) {
+      $hub_list = array();
+      foreach($hubs as $hub) {
+        $hub_list[] = $hub->name;
+      }
+      
+      $hub_output = implode(' | ', $hub_list);
+    }
+    
+    
     $map = get_field('map', $post);
     
     $data[] = array(
       'title' => $post->post_title,
       'url' => get_the_permalink($post),
-      'hub' => get_the_terms($post, 'hub')[0]->name,
+      'logo' => get_field('logo', $post)['url'],
+      
+      'hubs' => $hub_output,
+      'topics' => $topic_output,
+
+      'description' => apply_filters('the_content', get_post_field('post_content', $post->ID)),
+      
       'location' => array(
+        'address' => get_field('address_line_1', $post),
+        'city' => get_field('city', $post),
+        'province' => get_field('province', $post),
+        'postal_codes' => get_field('postal_codes', $post),
+        'country' => get_the_terms($post, 'country')[0]->name,
         'lat' => $map['markers'][0]['lat'],
         'lng' => $map['markers'][0]['lng'],
-        'label' => $map['markers'][0]['label'],
-        'country' => get_the_terms($post, 'country')[0]->name
+        'label' => $map['markers'][0]['label']
       ),
+
+      'contact' => array(
+        'email' => get_field('email', $post),
+        'website' => get_field('website', $post),
+        'twitter' => get_field('twitter', $post),
+        'facebook' => get_field('facebook', $post),
+        'instagram' => get_field('instagram', $post),
+        'youtube' => get_field('youtube', $post),
+        'additional_web_addresses' => get_field('additional_web_addresses', $post)
+      )
     );
   }
 
