b7762bd3fd6caa8b4a9fb35b3c149249844bce41
Mark Benewith <mark@benewith.com>
2020-05-18 15:31:13 +0100

    add initiative data export to CSV

--- templates/panels/hub-admin.php
+++ templates/panels/hub-admin.php
@@ -19,12 +19,16 @@
           <tr>
             <th>Hub Name</th>
             <th></th>
+            <th></th>
           </tr>
           <?php foreach ($hub_ids as $hub_id) {
             $hub = get_term_by('id', $hub_id, 'hub'); ?>
             <tr>
               <td>
                 <?php echo $hub->name; ?>
+              </td>
+              <td class="text-right">
+                <a class="btn btn-primary btn-sm" href="<?php echo add_query_arg('hub_id', $hub->term_id, parse_post_link(6105)); ?>"><?php echo svg('cloud-download'); ?>CSV of initiative data</a>
               </td>
               <td class="text-right">
                 <a class="btn btn-warning btn-sm" href="<?php echo add_query_arg('hub_id', $hub->term_id, parse_post_link(5414)); ?>"><?php echo svg('pencil'); ?>Edit</a>
@@ -35,5 +39,4 @@
       </div>
     </div>
   <?php } ?>
-
 </section>
