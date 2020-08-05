7e12f58cc043b0782d1e7fe050b9a597092964c0
Beef <mark@benewith.com>
2020-08-05 18:30:00 +0100

    create add new hub page

--- templates/content-page-add-new-hub.php
+++ templates/content-page-add-new-hub.php
@@ -3,6 +3,7 @@
     <?php while (have_posts()) : the_post(); ?>
       <h1><?php echo \Tofino\Helpers\title(); ?></h1>
       <?php the_content(); ?>
+      Add new hub form coming soon
     <?php endwhile; ?>
   </div>
 </main>
