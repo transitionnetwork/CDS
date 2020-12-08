510b223a65e40e7ffaa5afe5e654ea7baebeda68
Mark Benewith <mark@benewith.com>
2020-12-08 13:51:05 +0000

    reduce hub form width

--- templates/content-page-add-new-hub.php
+++ templates/content-page-add-new-hub.php
@@ -2,22 +2,26 @@
 <?php acf_form_head(); ?>
 <main>
   <div class="container">
-    <?php while (have_posts()) : the_post(); ?>
-      <h1><?php echo \Tofino\Helpers\title(); ?></h1>
+    <div class="row justify-content-center">	
+      <div class="col-12 col-md-10 col-lg-8">
+        <?php while (have_posts()) : the_post(); ?>
+          <h1><?php echo \Tofino\Helpers\title(); ?></h1>
 
-        <?php acf_form(array(
-          'post_id'		=> 'new_post',
-          'post_title'	=> true,
-          'post_content'	=> true,
-          'submit_value' => 'Send Request',
-          'uploader' => 'basic',
-          'updated_message' => __('Your request has been sent. Thank you.', 'tofino'),
-          'new_post'		=> array(
-            'post_type'		=> 'hub_applications',
-            'post_status'	=> 'draft'
-          ),
-        ));
-        echo '<div class="button-block"><a class="btn btn-secondary" href="javascript:history.go(-1)">Cancel</a></div>'; ?>
-    <?php endwhile; ?>
+            <?php acf_form(array(
+              'post_id'		=> 'new_post',
+              'post_title'	=> true,
+              'post_content'	=> true,
+              'submit_value' => 'Send Request',
+              'uploader' => 'basic',
+              'updated_message' => __('Your request has been sent. Thank you.', 'tofino'),
+              'new_post'		=> array(
+                'post_type'		=> 'hub_applications',
+                'post_status'	=> 'draft'
+              ),
+            ));
+            echo '<div class="button-block"><a class="btn btn-secondary" href="javascript:history.go(-1)">Cancel</a></div>'; ?>
+        <?php endwhile; ?>
+      </div>
+    </div>
   </div>
 </main>
