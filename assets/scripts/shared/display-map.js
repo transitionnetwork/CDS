eda77a7549069eaa318a2b93f5144c2eee3b3226
Mark Benewith <mark@benewith.com>
2021-04-20 12:42:38 +0100

    add not logged in initiative page

---
+++ assets/scripts/shared/display-map.js
@@ -0,0 +1,9 @@
+var $ = window.jQuery;
+import L from 'leaflet';
+import 'leaflet.markercluster';
+import 'leaflet-search';
+import 'leaflet.locatecontrol';
+
+export default function () {
+  
+}
--- templates/content-page-add-initiative.php
+++ templates/content-page-add-initiative.php
@@ -1,8 +1,3 @@
-<?php if (!is_user_logged_in()) {
-  wp_redirect (esc_url (add_query_arg ('error_code', '1', '/error')));
-  exit;
-} ?>
-
 <?php acf_form_head(); ?>
 
 <?php
@@ -12,26 +7,34 @@ if(is_user_role('initiative')) {
 } else {
   $post_status = 'publish';
 } ?>
+
 <main>
   <div class="container">
     <div class="row justify-content-center">	
       <div class="col-12 col-md-10 col-lg-8">
         <h1><?php echo \Tofino\Helpers\title(); ?></h1>
-        <?php acf_form(array(
-          'post_id'		=> 'new_post',
-          'post_title'	=> true,
-          'post_content'	=> true,
-          'return' => add_query_arg('added_post', 'initiative', '%post_url%'),
-          'submit_value' => 'Create Initiative',
-          'field_groups' => array ('group_5a26865e56e22', 'group_5a26865e64f00', 'group_5a26865e89711', 'group_5b3e27aee4439', 'group_606d979a4877d'),
-          'new_post'		=> array(
-            'post_type'		=> 'initiatives',
-            'post_status'	=> $post_status
-          ),
-        ));
-        echo '<div class="button-block"><a class="btn btn-secondary" href="javascript:history.go(-1)">Cancel</a></div>'; ?>
+
+        <?php if(!is_user_logged_in()) { ?>
+          <?php the_content(); ?>
+        <?php } else { ?>
+          <?php acf_form(array(
+            'post_id'		=> 'new_post',
+            'post_title'	=> true,
+            'post_content'	=> true,
+            'return' => add_query_arg('added_post', 'initiative', '%post_url%'),
+            'submit_value' => 'Create Initiative',
+            'uploader' => 'basic',
+            'new_post'		=> array(
+              'post_type'		=> 'initiatives',
+              'post_status'	=> $post_status
+            ),
+            'fields' => array ('hub_tax', 'logo', 'map', 'address_line_1', 'city', 'province', 'postal_code', 'country', 'email', 'website', 'twitter', 'facebook', 'instagram', 'youtube', 'additional_web_addresses', 'topic', 'private_email')
+          ));
+          echo '<div class="button-block"><a class="btn btn-secondary" href="javascript:history.go(-1)">Cancel</a></div>'; ?>
+        <?php } ?>
       </div>
     </div>
   </div>
 </main>
+
 <?php acf_enqueue_uploader(); ?>
