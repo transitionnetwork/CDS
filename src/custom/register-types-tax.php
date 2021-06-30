7bca386f8ded17889ff4451d78e59c7bb0933c7d
Mark Benewith <mark@benewith.com>
2021-06-30 14:42:14 +0100

    add topic to custom taxonomies

--- src/custom/register-types-tax.php
+++ src/custom/register-types-tax.php
@@ -200,5 +200,28 @@ function create_custom_taxonomies() {
   );
 
   register_taxonomy('trainer_type', array('trainers'), $args);
+
+  $args = array(
+    'hierarchical' => true,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'query_var' => true,
+    'rewrite' => array('slug' => 'trainer-topic', 'with_front' => false),
+    'labels' => array(
+      'name' => _x('Topics', 'taxonomy general name'),
+      'singular_name' => _x('Topic', 'taxonomy singular name'),
+      'search_items' => __('Search Topics'),
+      'all_items' => __('All Topics'),
+      'parent_item' => __('Parent Topic'),
+      'parent_item_colon' => __('Parent Topic:'),
+      'edit_item' => __('Edit Topic'),
+      'update_item' => __('Update Topic'),
+      'add_new_item' => __('Add New Topic'),
+      'new_item_name' => __('New Topic Name'),
+      'menu_name' => __('Topics'),
+    )
+  );
+
+  register_taxonomy('trainer_topic', array('trainers'), $args);
 }
 add_action('init', 'create_custom_taxonomies');
