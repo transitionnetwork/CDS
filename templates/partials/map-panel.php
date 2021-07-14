fb8518a41e6c7e56f67467df5badc3466b5f9e9a
Mark Benewith <mark@benewith.com>
2021-07-14 13:28:33 +0100

    repair link targets

--- templates/partials/map-panel.php
+++ templates/partials/map-panel.php
@@ -1,5 +1,5 @@
 <div id="map-info">
-  <a href="<?php echo home_url(); ?>" target="_top"><?php echo svg(array('sprite' => 'tn-logo', 'class' => 'logo')); ?></a>
+  <a href="<?php echo home_url(); ?>" target="_parent"><?php echo svg(array('sprite' => 'tn-logo', 'class' => 'logo')); ?></a>
   <div id="map-info-panel">
     <button class="close"><?php echo svg('x'); ?></button>
     <?php get_template_part('templates/partials/key'); ?>
