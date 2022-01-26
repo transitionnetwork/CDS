89e00f475c279e471f1ca4de4ac930363aeb5818
Mark Benewith <mark@benewith.com>
2022-01-26 16:58:37 +0000

    hid address and added some marker randomness to trainer page

--- templates/partials/single-map.php
+++ templates/partials/single-map.php
@@ -1,5 +1,16 @@
+<?php
+$lat = $map['lat'];
+$lng = $map['lng'];
+
+if(get_post_type() === 'trainers') {
+  $rand_small = mt_rand() / mt_getrandmax() / 30;
+  $lat -= $rand_small;
+  $lng -= $rand_small;
+} ?>
+
+
 <?php if($map && $map['markers']) { ?>
-  <div id="single-map" data-lat="<?php echo $map['lat']; ?>" data-lng="<?php echo $map['lng']; ?>" data-zoom="<?php echo $map['zoom']; ?>"></div>
+  <div id="single-map" data-lat="<?php echo $lat; ?>" data-lng="<?php echo $lng; ?>" data-zoom="<?php echo $map['zoom']; ?>"></div>
 <?php } ?>
 
 <?php if (get_field('address_line_1')) { ?>
@@ -10,7 +21,7 @@
   <?php echo get_field('postal_code'); ?><br/>
   <?php echo get_term_by('id', get_field('country'), 'country')->name; ?><br/>
   <div id="marker-address" data-address="<?php echo get_field('address_line_1'); ?>"></div>
-<?php } else if ($map && $map['markers']) { ?>
+<?php } else if ($map && $map['markers'] && get_post_type() !== 'trainers') { ?>
   <label><?php _e('Location', 'tofino'); ?></label>
   <div id="marker-address" data-address="<?php echo $map['markers'][0]['default_label']; ?>"></div>
   <?php echo $map['markers'][0]['default_label']; ?>
