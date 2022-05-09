b33e18e4c0fc8c0cd2becda34f8ae7b9b8eb3c08
Beef <mark@benewith.com>
2022-05-09 14:45:03 +0100

    set up export groups infrastructure

---
+++ template-group-export.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Template Name: Export - Group List
+ */
+
+
+$args = array(
+  'post_type' => 'initiatives',
+  'posts_per_page' => -1,
+  'orderby' => 'title',
+  'order' => 'ASC'
+);
+
+$posts = get_posts($args);
+die();
+
+$answers = array();
+$healthcheck_ids = array();
+
+$i = 0;
+foreach($posts as $post) {
+  // get associate initiave post object
+  $initiative = get_post($post->post_title);
+  
+  // only display the first ID and helathchecks for published initiatives;
+  if(!in_array($post->post_title, $healthcheck_ids) && $initiative->post_status == 'publish') {
+    $healthcheck_ids[] = $post->post_title;
+    $question_groups = get_fields($post->ID);
+    
+    $initiative_title = get_post($post->post_title);
+    
+    if($question_groups) {
+      $questions[0] = 'Initiative';
+      $answers[$i][0] = $initiative_title->post_title;
+      $answers[$i][1] = $post->ID;
+      
+      foreach($question_groups as $key => $group) {
+        // var_dump($group);
+        foreach ($group as $label => $response) {
+          // add question the first time
+          if($i == 0) {
+            $full_question = get_field_object($key . '_' . $label, 4642)['label'];
+            $questions[] = str_replace('group_', '', $key) . ('.') . str_replace('q', '', $label) . ': ' . $full_question;
+          }
+          $answers[$i][] = $response;
+        }
+      }
+    }
+  }
+  $i++;
+}
+
+$export_data[] = $questions;
+foreach($answers as $answer_set) {
+  $export_data[] = $answer_set;
+}
+
+//secure the export
+if(is_user_role('administrator') || is_user_role('super_hub')) {
+  outputCsv(date('Ymd') . '_healthcheck_responses.csv', $export_data);
+} else {
+  wp_redirect(esc_url(add_query_arg('error_code', '1', '/error')));
+} ?>
--- templates/panels/reporting.php
+++ templates/panels/reporting.php
@@ -3,5 +3,6 @@
   <ul>
     <li><a href="<?php echo parse_post_link(4829); ?>"><?php _e('Download current healthcheck responses', 'tofino'); ?></a></li>
     <li><a href="<?php echo parse_post_link(4827); ?>"><?php _e('List groups by healthcheck date', 'tofino'); ?></a></li>
+    <li><a href="<?php echo parse_post_link(7336); ?>"><?php _e('Download CSV of all groups', 'tofino'); ?></a></li>
   </ul>
 </section>
