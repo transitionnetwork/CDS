a4d80cab3b6ec1f27daefac521d8f749d3205d5a
Mark Benewith <mark@benewith.com>
2022-05-10 10:39:32 +0100

    complete group export data generation

--- template-group-export.php
+++ template-group-export.php
@@ -12,52 +12,102 @@ $args = array(
 );
 
 $posts = get_posts($args);
-die();
 
-$answers = array();
-$healthcheck_ids = array();
+$column_titles = array(
+  'ID',
+  'Name',
+  'Hub',
+  'Topics',
+  'Country',
+  'Location',
+  'Description',
+  'Email',
+  'Website URL',
+  'Twitter URL',
+  'Facebook URL',
+  'Instagram URL',
+  'Youtube URL'
+);
 
-$i = 0;
-foreach($posts as $post) {
-  // get associate initiave post object
-  $initiative = get_post($post->post_title);
+$export_data = array();
+$export_data[] = $column_titles;
+
+foreach($posts as $key => $post) {
+  setup_postdata($post);
   
-  // only display the first ID and helathchecks for published initiatives;
-  if(!in_array($post->post_title, $healthcheck_ids) && $initiative->post_status == 'publish') {
-    $healthcheck_ids[] = $post->post_title;
-    $question_groups = get_fields($post->ID);
-    
-    $initiative_title = get_post($post->post_title);
-    
-    if($question_groups) {
-      $questions[0] = 'Initiative';
-      $answers[$i][0] = $initiative_title->post_title;
-      $answers[$i][1] = $post->ID;
-      
-      foreach($question_groups as $key => $group) {
-        // var_dump($group);
-        foreach ($group as $label => $response) {
-          // add question the first time
-          if($i == 0) {
-            $full_question = get_field_object($key . '_' . $label, 4642)['label'];
-            $questions[] = str_replace('group_', '', $key) . ('.') . str_replace('q', '', $label) . ': ' . $full_question;
-          }
-          $answers[$i][] = $response;
-        }
-      }
-    }
+  $hubs = wp_get_post_terms($post->ID, 'hub');
+  if($hubs) {
+    $hub = $hubs[0]->name;
+  } else {
+    $hub = null;
   }
-  $i++;
-}
 
-$export_data[] = $questions;
-foreach($answers as $answer_set) {
-  $export_data[] = $answer_set;
+  $countries = wp_get_post_terms($post->ID, 'country');
+  if($countries) {
+    $country = $countries[0]->name;
+  } else {
+    $country = null;
+  }
+
+  $topics = get_the_terms($post, 'topic');
+  if($topics) {
+    $topic_names = [];
+    foreach ($topics as $topic) {
+      $topic_names[] = $topic->name;
+    }
+    $topic_names = implode(', ', $topic_names);
+  } else {
+    $topic_names = null;
+  }
+
+  //Location
+  $location = null;
+  if (get_field('address_line_1')) {
+    $location = array();
+
+    if(get_field('address_line_1')) {
+      $location[] = get_field('address_line_1');
+    }
+
+    if(get_field('city')) {
+      $location[] = get_field('city');
+    }
+
+    if(get_field('province')) {
+      $location[] = get_field('province');
+    }
+
+    if(get_field('postal_code')) {
+      $location[] = get_field('postal_code');
+    }
+
+    $location = implode(', ', $location);
+  } else if (get_field('map') && get_field('map')['markers']) {
+    $location = get_field('map')['markers'][0]['default_label'];
+  }
+
+  $export_data[] = array(
+    get_the_ID(),
+    get_the_title(),
+    $hub,
+    $topic_names,
+    $country,
+    trim($location),
+    get_the_content(),
+    get_field('email'),
+    get_field('website'),
+    get_field('twitter'),
+    get_field('facebook'),
+    get_field('instagram'),
+    get_field('youtube'),
+  );
+
+  wp_reset_postdata();
 }
 
 //secure the export
 if(is_user_role('administrator') || is_user_role('super_hub')) {
-  outputCsv(date('Ymd') . '_healthcheck_responses.csv', $export_data);
+  outputCsv(date('Ymd') . '_transition_groups_export.csv', $export_data);
 } else {
   wp_redirect(esc_url(add_query_arg('error_code', '1', '/error')));
 } ?>
