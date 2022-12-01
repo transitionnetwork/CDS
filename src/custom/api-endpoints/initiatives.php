ea142d4d3016e92b046c3cae703c656f830324e2
Beef <mark@benewith.com>
2022-12-01 18:07:47 +0000

    amend authorised API endpoint

--- src/custom/api-endpoints/initiatives.php
+++ src/custom/api-endpoints/initiatives.php
@@ -1,5 +1,5 @@
 <?php
-function get_group_data($post, $callback = null) {
+function get_group_data($post) {
   $logo = get_field('logo', $post);
   $logo = ($logo && $logo['type'] === 'image') ? $logo['sizes']['large'] : '';
 
@@ -19,10 +19,36 @@ function get_group_data($post, $callback = null) {
     'contact' => endpoint_get_contact($post),
     'last_updated' => get_the_modified_date('Y-m-d H:i:s', $post)
   );
+  
+  return $data;
+}
 
-  if(isset($callback) && $callback === 'endpoint_get_groups_full_info') {
-    $data['private_email'] = get_field('private_email', $post);
-  }
+function get_full_group_data($post) {
+  $logo = get_field('logo', $post);
+  $logo = ($logo && $logo['type'] === 'image') ? $logo['sizes']['large'] : '';
+
+  $data = array(
+    'group_id' => $post->ID,
+    'private_email' => get_field('private_email', $post),
+    'url' => get_the_permalink($post),
+    'logo' => $logo,
+    'hubs' => endpoint_get_taxonomy_terms($post, 'hub'),
+    'countries' => endpoint_get_taxonomy_terms($post, 'country'),
+    'topics' => endpoint_get_taxonomy_terms($post, 'topic'),
+    'description' => apply_filters('the_content', get_post_field('post_content', $post->ID)),
+
+    'location_address' => get_field('address_line_1', $post),
+    'location_city' => get_field('city', $post),
+    'location_province' => get_field('province', $post),
+    'location_postcode' => get_field('postal_codes', $post),
+    'location_country' => (get_the_terms($post, 'country')) ? get_the_terms($post, 'country')[0]->name : '',
+
+    'contact_website' => get_field('website', $post),
+    'contact_twitter' => get_field('twitter', $post),
+    'contact_facebook' => get_field('facebook', $post),
+    'contact_instagram' => get_field('instagram', $post),
+    'contact_youtube' => get_field('youtube', $post),
+  );
   
   return $data;
 }
@@ -50,7 +76,12 @@ function get_groups_from_request($request) {
   if($post_query->have_posts()) {
     while($post_query->have_posts()) : $post_query->the_post();
       global $post;
-      $data[] = get_group_data($post, $request_attributes['callback']);
+
+      if(isset($request_attributes) && $request_attributes['callback'] === 'endpoint_get_groups_full_info') {
+        $data[] = get_full_group_data($post);
+      } else {
+        $data[] = get_group_data($post);
+      }
     endwhile;
   }
 
