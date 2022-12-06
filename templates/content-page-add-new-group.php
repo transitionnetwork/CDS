dddce588f1296d6f7085daf0e08663dcad0268da
Beef <mark@benewith.com>
2022-12-06 15:16:58 +0000

    remove post_content from add form

--- templates/content-page-add-new-group.php
+++ templates/content-page-add-new-group.php
@@ -22,7 +22,6 @@ if(is_user_role('initiative')) {
           <?php acf_form(array(
             'post_id'		=> 'new_post',
             'post_title'	=> true,
-            'post_content'	=> true,
             'return' => add_query_arg('added_post', 'initiative', '%post_url%'),
             'submit_value' => 'Create Group',
             // 'field_groups' => array ('group_5a26865e56e22', 'group_5a26865e64f00', 'group_5a26865e89711', 'group_5b3e27aee4439', 'group_606d979a4877d'),
