7f6bc5b7eeb596c397988076a7c985571c56ac41
Beef <mark@benewith.com>
2023-03-03 11:35:37 +0000

    further UI tweaks

--- templates/partials/update-author.php
+++ templates/partials/update-author.php
@@ -1,5 +1,5 @@
 <?php $post_author_id = get_the_author_meta('ID'); ?>
-<div class="panel mt-4">
+<div class="panel">
   <h3>Author</h3>
   <label>Name</label><?php echo get_the_author_meta('display_name'); ?>
   <label>Email</label><a href="mailto:<?php echo get_the_author_meta('user_email'); ?>"><?php echo get_the_author_meta('user_email'); ?></a>
@@ -26,24 +26,22 @@ $users = get_users($args);
 
 <?php if($users) { ?>
   <form action="<?php the_permalink() ?>" method="POST" id="change-author" class="panel">
-    <label for="authors">Update author</label>
-
-
-    <p>
+    <h3 for="authors">Update author</h3>
+    <div class="mt-3">
       <select name="authors">
         <?php foreach($users as $user) { ?>
           <option value="<?php echo $user->ID; ?>" <?php echo ($user->ID === $post_author_id) ? 'selected' : ''; ?>><?php echo $user->display_name; ?> | <?php echo $user->user_email; ?></option>
         <?php } ?>
       </select>
       <input name="post_id" type="hidden" value="<?php echo $post->ID; ?>">
-    </p>
-    <p>
-      <input type="submit" value="Change">
-    </p>
+    </div>
+    <input type="submit" value="Change" class="mt-3">
   </form>
 <?php } ?>
 
 <?php if(get_field('private_email')) { ?>
-  <label><?php echo get_field_object('private_email')['label']; ?>:</label>
-  <a href="mailto:<?php echo get_field('private_email'); ?>"><?php echo get_field('private_email'); ?></a>
+  <div class="panel">
+    <h3><?php echo get_field_object('private_email')['label']; ?>:</h3>
+    <a href="mailto:<?php echo get_field('private_email'); ?>"><?php echo get_field('private_email'); ?></a>
+  </div>
 <?php } ?>
