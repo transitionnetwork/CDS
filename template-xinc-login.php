cdfc8f01466d5018cd36f4ae7de0b398ee94831d
Beef <mark@benewith.com>
2023-03-24 11:39:28 +0000

    add openID to front end and style button

--- template-xinc-login.php
+++ template-xinc-login.php
@@ -13,10 +13,8 @@
       <?php while (have_posts()) : the_post(); ?>
         <h1><?php echo \Tofino\Helpers\title(); ?></h1>
 
-        <?php echo do_shortcode('[openid_connect_generic_login_button]'); ?>
-        <p><a class="btn btn-outline" href="https://id.transition-space.org/realms/cloudron/protocol/openid-connect/auth?response_type=code&amp;scope=email%20profile%20openid%20offline_access&amp;client_id=groups.stage.xinc.digital&amp;state=6ef34fa48044e1eb6b7e48ceb67ef062&amp;redirect_uri=https%3A%2F%2Fgroups.stage.xinc.digital%2Fwp-admin%2Fadmin-ajax.php%3Faction%3Dopenid-connect-authorize">Login with OpenID Connect</a></p>
-          <?php if(is_plugin_active('daggerhart-openid-connect-generic')) { ?>
-            <p><a class="btn btn-outline" href="https://id.transition-space.org/realms/cloudron/protocol/openid-connect/auth?response_type=code&amp;scope=email%20profile%20openid%20offline_access&amp;client_id=groups.stage.xinc.digital&amp;state=6ef34fa48044e1eb6b7e48ceb67ef062&amp;redirect_uri=https%3A%2F%2Fgroups.stage.xinc.digital%2Fwp-admin%2Fadmin-ajax.php%3Faction%3Dopenid-connect-authorize">Login with OpenID Connect</a></p>
+        <?php if(is_plugin_active('daggerhart-openid-connect-generic/openid-connect-generic.php') && !is_user_logged_in()) { ?>
+          <?php echo do_shortcode('[openid_connect_generic_login_button]'); ?>
         <?php } ?>
 
         <?php the_content(); ?>
