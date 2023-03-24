d54c392c06b974260a89cc4b561341211fc20c30
Beef <mark@benewith.com>
2023-03-24 10:06:01 +0000

    remove plugin active conditional for testing

--- template-xinc-login.php
+++ template-xinc-login.php
@@ -13,9 +13,7 @@
       <?php while (have_posts()) : the_post(); ?>
         <h1><?php echo \Tofino\Helpers\title(); ?></h1>
 
-          <?php if(is_plugin_active('daggerhart-openid-connect-generic')) { ?>
-            <p><a class="btn btn-ouline" href="https://id.transition-space.org/realms/cloudron/protocol/openid-connect/auth?response_type=code&amp;scope=email%20profile%20openid%20offline_access&amp;client_id=groups.stage.xinc.digital&amp;state=6ef34fa48044e1eb6b7e48ceb67ef062&amp;redirect_uri=https%3A%2F%2Fgroups.stage.xinc.digital%2Fwp-admin%2Fadmin-ajax.php%3Faction%3Dopenid-connect-authorize">Login with OpenID Connect</a></p>
-        <?php } ?>
+          <p><a class="btn btn-ouline" href="https://id.transition-space.org/realms/cloudron/protocol/openid-connect/auth?response_type=code&amp;scope=email%20profile%20openid%20offline_access&amp;client_id=groups.stage.xinc.digital&amp;state=6ef34fa48044e1eb6b7e48ceb67ef062&amp;redirect_uri=https%3A%2F%2Fgroups.stage.xinc.digital%2Fwp-admin%2Fadmin-ajax.php%3Faction%3Dopenid-connect-authorize">Login with OpenID Connect</a></p>
 
         <?php the_content(); ?>
       <?php endwhile; ?>
