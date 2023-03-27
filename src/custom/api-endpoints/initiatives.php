8d84b84699e0847983816bc674c3c0045f8c7351
Beef <mark@benewith.com>
2023-03-27 11:16:44 +0100

    add public_email data to all groups data

--- src/custom/api-endpoints/initiatives.php
+++ src/custom/api-endpoints/initiatives.php
@@ -32,6 +32,7 @@ function get_full_group_data($post) {
     'title' => $post->post_title,
     'group_id' => $post->ID,
     'baserow_id' => (int)get_post_meta( $post->ID, 'baserow_id', true ),
+    'public_email' => get_field('email', $post),
     'private_email' => get_field('private_email', $post),
     'user_email' => get_the_author_meta('user_email', $post->post_author),
     'url' => get_the_permalink($post),
