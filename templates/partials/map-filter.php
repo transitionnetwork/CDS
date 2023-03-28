6517ab98022e78bee2f93508988b68707076e467
Beef <mark@benewith.com>
2023-03-28 10:29:24 +0100

    add labels to map widget selects

--- templates/partials/map-filter.php
+++ templates/partials/map-filter.php
@@ -4,7 +4,7 @@
       <div class="col-12 col-md-3 filter-col">
         <div id="filter-type" class="filter-item">
           <label for="type_name"><?php _e('Show:', 'tofino'); ?></label>
-          <select name="type_name">
+          <select name="type_name" id="type_name">
             <option value="1" <?php echo (get_query_var('type') === "1") ? 'selected' : null; ?>>All</option>
             <option value="2" <?php echo (get_query_var('type') === "2") ? 'selected' : null; ?>>Groups</option>
             <option value="3" <?php echo (get_query_var('type') === "3") ? 'selected' : null; ?>>Hubs</option>
@@ -22,7 +22,7 @@
         
         <div id="filter-hub" class="filter-item">
           <label for="hub_name"><?php _e('Hub:', 'tofino'); ?></label>
-          <select name="hub_name">
+          <select name="hub_name" id="hub_name">
             <option value="">All</option>
             
             <?php foreach ($terms as $term) { ?>
@@ -40,7 +40,7 @@
         <?php $terms = get_terms('country'); ?>
         <div id="filter-country" class="filter-item">
           <label for="country"><?php _e('Country:', 'tofino'); ?></label>
-          <select name="country">
+          <select name="country" id="country">
             <option value="">All</option>
             <?php foreach ($terms as $term) { ?>
               <?php if (get_query_var('country') == $term->slug) {
