ad69ef3e951838ec2647104f8af6cb764da137af
Beef <mark@benewith.com>
2023-03-28 10:49:59 +0100

    alt text label fixes

--- templates/partials/key.php
+++ templates/partials/key.php
@@ -1,16 +1,16 @@
 <ul class="key">
   <li class="initiative">
-    <img src="<?php echo get_template_directory_uri() . '/dist/img/icons/marker-icon-initiative-2x.png'; ?>">
+    <img src="<?php echo get_template_directory_uri() . '/dist/img/icons/marker-icon-initiative-2x.png'; ?>" alt="Group Marker">
     <?php _e('Groups'); ?>
     <a class="count" href="<?php echo home_url(); ?>" title="View Groups">(0)</a>
   </li>
   <li class="hub">
-    <img src="<?php echo get_template_directory_uri() . '/dist/img/icons/marker-icon-hub-2x.png'; ?>">
+    <img src="<?php echo get_template_directory_uri() . '/dist/img/icons/marker-icon-hub-2x.png'; ?>" alt="Hub Marker">
     <?php _e('Hubs'); ?>
     <a class="count" href="<?php echo home_url('hub-list'); ?>" title="View Hubs">(0)</a>
   </li>
   <li class="trainer">
-    <img src="<?php echo get_template_directory_uri() . '/dist/img/icons/marker-icon-trainer-2x.png'; ?>">
+    <img src="<?php echo get_template_directory_uri() . '/dist/img/icons/marker-icon-trainer-2x.png'; ?>" alt="Trainer Marker">
     <?php _e('Trainers'); ?>
     <a class="count" href="<?php echo home_url('trainers'); ?>" title="View Trainers">(0)</a>
   </li>
