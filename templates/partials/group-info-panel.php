14eacdf2c3e35d302575911861a027c176e714cb
Beef <mark@benewith.com>
2023-04-26 13:00:44 +0100

    move order of info

--- templates/partials/group-info-panel.php
+++ templates/partials/group-info-panel.php
@@ -1,3 +1,56 @@
+<?php $email = get_field('email'); ?>
+<?php $additional = get_field('additional_web_addresses'); ?>
+<?php
+$link_fields = array(
+  'website',
+  'twitter',
+  'facebook',
+  'instagram',
+  'youtube',
+);
+
+$has_links = false;
+
+foreach($link_fields as $field) {
+  if(get_field($field)) {
+    $has_links = true;
+  }
+} ?>
+
+<?php if($email || $has_links || $additional) { ?>
+  <div class="panel">
+    <?php if ($email) { ?>
+      <h3>Email</h3>
+      <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
+    <?php } ?>
+  
+    <?php if($has_links) { ?>
+      <div>
+        <h3 class="mt-3"><?php _e('Links', 'tofino'); ?></h3>
+        
+        <ul class="links">
+          <?php foreach($link_fields as $field) {
+            if(get_field($field)) { ?>
+              <li><a href="<?php echo get_field($field); ?>" target="_blank"><?php echo svg($field); ?></a></li>
+            <?php } ?>
+          <?php } ?>
+        </ul>
+      </div>
+    <?php } ?>
+    
+    <?php if($additional) { ?>
+      <div>
+        <h3 class="mt-3"><?php _e('More Links', 'tofino'); ?></h3>
+        <ul>
+          <?php foreach($additional as $item) { ?>
+            <li><a href="<?php echo $item['address']; ?>" target="_blank"><?php echo $item['label']; ?></a></li>
+          <?php } ?>
+        </ul>
+      </div>
+    <?php } ?>
+  </div>
+<?php } ?>
+
 <?php $hubs = get_the_terms($post, 'hub'); ?>
 <?php $topics = get_the_terms($post, 'topic'); ?>
 
@@ -23,53 +76,3 @@
     <?php } ?>
   </div>
 <?php } ?>
-
-<div class="panel">
-  <?php if (get_field('email')) { ?>
-    <h3 class="mt-3">Email</h3>
-    <a href="mailto:<?php echo get_field('email'); ?>"><?php echo get_field('email'); ?></a>
-  <?php } ?>
-
-  <?php
-  $link_fields = array(
-    'website',
-    'twitter',
-    'facebook',
-    'instagram',
-    'youtube',
-  );
-
-  $has_links = false;
-
-  foreach($link_fields as $field) {
-    if(get_field($field)) {
-      $has_links = true;
-    }
-  } ?>
-
-  <?php if($has_links) { ?>
-    <div>
-      <h3 class="mt-3"><?php _e('Links', 'tofino'); ?></h3>
-      
-      <ul class="links">
-        <?php foreach($link_fields as $field) {
-          if(get_field($field)) { ?>
-            <li><a href="<?php echo get_field($field); ?>" target="_blank"><?php echo svg($field); ?></a></li>
-          <?php } ?>
-        <?php } ?>
-      </ul>
-    </div>
-  <?php } ?>
-
-  <?php $additional = get_field('additional_web_addresses'); 
-  if($additional) { ?>
-    <div>
-      <h3 class="mt-3"><?php _e('More Links', 'tofino'); ?></h3>
-      <ul>
-        <?php foreach($additional as $item) { ?>
-          <li><a href="<?php echo $item['address']; ?>" target="_blank"><?php echo $item['label']; ?></a></li>
-        <?php } ?>
-      </ul>
-    </div>
-  <?php } ?>
-</div>
