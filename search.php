67c7e2d5d8d12be38092ebb66cc7b1a3a503b3c1
Beef <mark@benewith.com>
2023-04-26 10:49:26 +0100

    show only groups results in search

--- search.php
+++ search.php
@@ -9,27 +9,9 @@
       </div>
       
       <div class="col-12 col-lg-9 order-lg-1">
-        <?php if (get_query_var('hub_name')) :
-          $term = get_term_by('slug', get_query_var('hub_name'), 'hub');
-          echo '<h1>Hub: ' . $term->name . '</h1>';
-          echo $term->description;
-        endif; ?>
-        
-        <?php if (get_query_var('country')) :
-          $term = get_term_by('slug', get_query_var('country'), 'country');
-          echo '<h1>Country: ' . $term->name . '</h1>';
-          echo $term->description;
-        endif; ?>
+
     
         <h1><?php echo \Tofino\Helpers\title(); ?></h1>
-
-        <?php while (have_posts()) : the_post(); ?>
-          <?php if (!is_user_logged_in() && !empty(get_the_content())) { ?>
-            <div class="panel mt-3">
-              <?php the_content(); ?>
-            </div>
-          <?php } ?>
-        <?php endwhile; ?>
 
         <?php $init_query = get_initiatives_main(); ?>
         <?php set_query_var('init_query', $init_query); ?>
