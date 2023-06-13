efc047ce92b94faf2486bc371fd0b951cebb4e62
Beef <mark@benewith.com>
2023-06-13 14:25:59 +0100

    add trainer profile images to og data

--- src/custom/rank-math.php
+++ src/custom/rank-math.php
@@ -1,52 +1,70 @@
 <?php
+
+function get_custom_og_image() {
+  if (is_singular('initiatives')) {
+    return get_field('logo');
+  }
+  
+  if(is_singular('trainers')) {
+    return get_field('general_information_trainer_photo');
+  }
+  
+  return false;
+}
 /**
  * Hook to manually modify og:tags
  */
 add_action( 'rank_math/head', function() {
-  if(is_singular('initiatives')) {
+  if(is_singular('initiatives') || is_singular('trainers')) {
     add_filter( "rank_math/opengraph/facebook/og_image", function($data) {
-      if(get_field('logo')) {
-        return get_field('logo')['url'];
+      $image_field = get_custom_og_image();
+      if($image_field) {
+        return $image_field['url'];
       } else {
         return $data;
       }
     });
     
     add_filter( "rank_math/opengraph/facebook/og_image_secure_url", function($data) {
-      if(get_field('logo')) {
-        return get_field('logo')['url'];
+      $image_field = get_custom_og_image();
+      if($image_field) {
+        return $image_field['url'];
       } else {
         return $data;
       }
     });
 
     add_filter( "rank_math/opengraph/facebook/og_image_width", function($data) {
-      if(get_field('logo')) {
-        return get_field('logo')['width'];
+      $image_field = get_custom_og_image();
+      if($image_field) {
+        return $image_field['width'];
       } else {
         return $data;
       }
     });
     
     add_filter( "rank_math/opengraph/facebook/og_image_height", function($data) {
-      if(get_field('logo')) {
-        return get_field('logo')['height'];
+      $image_field = get_custom_og_image();
+      if($image_field) {
+        return $image_field['height'];
       } else {
         return $data;
       }
     });
     
     add_filter( "rank_math/opengraph/facebook/og_image_type", function($data) {
-      if(get_field('logo')) {
-        return get_field('logo')['mime_type'];
+      $image_field = get_custom_og_image();
+      if($image_field) {
+        return $image_field['mime_type'];
       } else {
         return $data;
       }
     });
     
     add_filter( "rank_math/opengraph/twitter/twitter_image", function($data) {
-      if(get_field('logo')) {
-        return get_field('logo')['url'];
+      $image_field = get_custom_og_image();
+      if($image_field) {
+        return $image_field['url'];
       } else {
         return $data;
       }
