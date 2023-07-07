5988bb49d7ffc90e3c140af9c8c2d19348f1e53f
Beef <mark@benewith.com>
2023-07-07 13:24:31 +0100

    complete webhooks work v1

--- src/custom/api-endpoints/webhooks.php
+++ src/custom/api-endpoints/webhooks.php
@@ -8,11 +8,29 @@ function add_webhook_endpoints() {
 add_action( 'rest_api_init', 'add_webhook_endpoints');
 
 function process_mailgun_endpoint(WP_REST_Request $request) {
-  $attributes = $request->get_attributes();
-  $json = $request->get_json_params();
   $body = $request->get_body();
-  var_dump($request);
-  var_dump($attributes);
-  var_dump($json);
-  var_dump($body);
+  $body = json_decode($body, TRUE);
+
+  
+  $query_params = $request->get_query_params();
+  $event_type = array_key_first($query_params);
+  
+  $post_id = (int)$body['event-data']['user-variables']['post_id'];
+
+  if($post_id) {
+    //set mail_event field to delivered or failure for query sakes
+    if(in_array($event_type, array('delivered', 'failure', 'opens', 'clicks'))) {
+      update_post_meta( $post_id, 'mail_event', $event_type);
+    }
+  
+    $mail_log = get_post_meta($post_id, 'mail_event', true);
+    if(!is_array($mail_log)) {
+      $mail_log = array();
+    }
+    
+    $mail_log[] = date('Y-m-d H:i:s') . ' - ' . $event_type;
+    update_post_meta( $post->ID, 'mail_event', $mail_log);
+  }
+
+  return;
 }
