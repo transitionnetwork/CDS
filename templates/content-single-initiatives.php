dddd759776fe3d2afb5a9d61d418e65b19b7dae5
Beef <mark@benewith.com>
2023-07-10 15:34:35 +0100

    logging of emails now displaying in CRM

--- templates/content-single-initiatives.php
+++ templates/content-single-initiatives.php
@@ -143,6 +143,24 @@
               </p>
             </div>
           
+            <div class="panel">
+              <h3>Email History</h3>
+              <?php $email_log = get_post_meta( $post->ID, 'mail_log', true); ?>
+
+              <?php if($email_log) { ?>
+                <?php $email_log = array_reverse($email_log); ?>
+                <div class="panel">
+                  <div class="mb-3">
+                    <strong>Last Event:</strong> <?php echo get_post_meta($post->ID, 'last_mail_event', true); ?>
+                  </div>
+                  <?php foreach($email_log as $item) { ?>
+                    <?php echo $item; ?></br>
+                  <?php } ?>
+                </div>
+
+              <?php } else { ?>
+              <?php } ?>
+            </div>
           <?php } ?>
 
           <?php if (can_view_healthcheck($post)) { ?>
