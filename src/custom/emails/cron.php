7386b5581356db422af1adf742b38009a4a14b59
Beef <mark@benewith.com>
2023-09-13 15:52:36 +0100

    fix cron naming

--- src/custom/emails/cron.php
+++ src/custom/emails/cron.php
@@ -23,11 +23,11 @@ add_action('email_pending_groups_hook', 'check_pending_groups');
 //
 
 //check for inactive authors
-if (!wp_next_scheduled('email_inactive_authors')) {
-  wp_schedule_event(time(), 'four_days', 'email_inactive_authors');
+if (!wp_next_scheduled('email_inactive_authors_hook')) {
+  wp_schedule_event(time(), 'four_days', 'email_inactive_authors_hook');
 }
 
-add_action('email_inactive_authors', 'check_inactive_authors');
+add_action('email_inactive_authors_hook', 'email_inactive_authors');
 
 
 function email_inactive_authors() {
