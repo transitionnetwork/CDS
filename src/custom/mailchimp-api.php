afe2d2565712db5ddc251c734b9748565a8999ce
Beef <mark@benewith.com>
2023-10-25 17:17:59 +0100

    add mailchimp subscription to new group authors

---
+++ src/custom/mailchimp-api.php
@@ -0,0 +1,22 @@
+<?php
+function mailchimp_subscribe_author($post) {
+  $hub_mailchimp_audience_map = array(
+    800 => 'edd02f822a', // London and SE
+    284 => '636863f48c' // England and Wales
+  );
+
+  $author = get_userdata( $post->post_author);
+
+  require_once TEMPLATEPATH . '/vendor/autoload.php';
+  
+  $client = new MailchimpMarketing\ApiClient();
+  $client->setConfig([
+      'apiKey' => 'f05fe9940c9efe4a1828104b45410ed5-us16',
+      'server' => 'us16',
+  ]);
+  
+  $response = $client->lists->addListMember($hub_mailchimp_audience_map[800], [
+      "email_address" => $author->user_email,
+      "status" => "pending",
+  ]);
+}
