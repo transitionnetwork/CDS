bdb98d98bb9676a2147a637a21891b1e459b330d
Beef <mark@benewith.com>
2023-11-13 15:47:34 +0000

    v1 of events display

--- src/xinc-events/events.php
+++ src/xinc-events/events.php
@@ -7,49 +7,91 @@ function xinc_events_register() {
   }
 }
 
-function xinc_events_list_events() {
-  
-  $token = 'Token ' . 'pb5jpclkev7921ba03jxigs3574vl6alkdmblbjmcyr21gh1ggghe8mla3ol2and';
-  $api_url = 'https://pretix.eu/api/v1/organizers/transition-network/events/?is_future=true';
-  
-  $response = wp_remote_get( $api_url, array(
-    'headers' => array(
-      'Content-Type'  => 'application/json',
-      'Authorization' => $token
-    )
-  ) );
-  
-  if ( is_array( $response ) && ! is_wp_error( $response ) ) {
-    // $headers = $response['headers']; // array of http header lines
-    $body    = json_decode($response['body']); // use the content
-  }
-  
-  $results = $body->results;
-  
-  if($results) {
-    foreach($results as $result) {
-      // var_dump($result);
-      $api_url = 'https://pretix.eu/api/v1/organizers/transition-network/events/' . $result->slug . '/settings?explain=true';
-      $response = wp_remote_get( $api_url, array(
-        'headers' => array(
-          'Content-Type'  => 'application/json',
-          'Authorization' => $token
-        )
-      ) );
-      
-      if ( is_array( $response ) && ! is_wp_error( $response ) ) {
-        // $headers = $response['headers']; // array of http header lines
-        $body = json_decode($response['body']); // use the content
-        ?>
-  
-        <img src="<?php echo $body->logo_image->value; ?>" alt="<?php echo $result->name->en; ?>">
-        <?php echo $body->frontpage_text->value->en; ?>
-        <a href="<?php echo $result->public_url; ?>" target="_blank">Buy tickets</a>
-  
-      <?php }
+xinc_events_register();
+
+function xinc_events_get_events($page = 1) {
+  $time = (time() - (time() % 3600)); // rounded to nearest hour;
+  $path = TEMPLATEPATH . '/cache/events-' . $time . '-' . $page .'.json';
+
+  if(!file_exists($path)) {
+    $token = 'Token ' . get_field('pretix_api_token', 'options');
+    
+    $api_url = 'https://pretix.eu/api/v1/organizers/transition-network/events/?is_future=true&ordering=date_from';
+    
+    $response = wp_remote_get( $api_url, array(
+      'headers' => array(
+        'Content-Type'  => 'application/json',
+        'Authorization' => $token
+      )
+    ) );
+    
+    if ( is_array( $response ) && ! is_wp_error( $response ) ) {
+      // $headers = $response['headers']; // array of http header lines
+      $body    = json_decode($response['body']); // use the content
     }
+  
+    $results = $body->results;
+  
+    if($results) {
+  
+      $output = array();
+      
+      foreach($results as $result) {
+        // var_dump($result);
+        $api_url = 'https://pretix.eu/api/v1/organizers/transition-network/events/' . $result->slug . '/settings?explain=true';
+        $response = wp_remote_get( $api_url, array(
+          'headers' => array(
+            'Content-Type'  => 'application/json',
+            'Authorization' => $token
+          )
+        ) );
+        
+        if ( is_array( $response ) && ! is_wp_error( $response ) ) {
+  
+          $body = json_decode($response['body']);
+          // var_dump($body);
+          
+          $output[] = array(
+            'title' => $result->name->en,
+            'event_url' => $result->public_url,
+            'date_from' => $result->date_from,
+            'date_to' => $result->date_to,
+            'location' => $result->location->en,
+            'geo_lat' => $result->geo_lat,
+            'geo_lng' => $result->geo_lng,
+            'description' => $body->frontpage_text->value->en,
+            'image_url' => $body->logo_image->value,
+          );
+        }
+      }
+  
+      file_put_contents($path, json_encode($output));
+      
+      return $output;
+    }
+  
+  } else {
+    return json_decode(file_get_contents($path), true);
   }
+
+  return;
 }
 
-xinc_events_register();
+function xinc_events_first_words($sentence, $words = 10) {
+  return implode(' ', array_slice(explode(' ', $sentence), 0, $words));
+}
+
+function xinc_events_link_urls($sentence) {
+  $words = explode(' ', $sentence);
+  $output = array();
+  foreach($words as $word) {
+    if(filter_var($word, FILTER_VALIDATE_URL) === FALSE) {
+      $output[] = $word;
+    } else {
+      $output[] = '<a href="' . $word . '" target="_blank">'. $word . '</a>';
+    }
+  }
+
+  return implode(' ', $output);
+}
 ?>
--- templates/content-page-events.php
+++ templates/content-page-events.php
@@ -2,6 +2,64 @@
   <div class="container">
     <?php while (have_posts()) : the_post(); ?>
       <h1><?php echo \Tofino\Helpers\title(); ?></h1>
-    <?php endwhile; ?>
+
+      <?php $events = xinc_events_get_events(); ?>
+
+      <?php if($events) { ?>
+        <div class="xinc-events__event_list">
+          <?php foreach($events as $event) { ?>
+            <div class="xinc-events__event_list__event_tile">
+              <div class="row">
+                <?php if($event['image_url']) { ?>
+                  <div class="col-12 col-md-4">
+                    <img src="<?php echo $event['image_url']; ?>" alt="<?php echo $event['title']; ?>" title="<?php echo $event['title']; ?>">
+                  </div>
+                <?php } ?>
+                <div class="col-12 col-md-8">
+                  <div class="xinc-events__event_list__event_tile__details">
+                    <h3>
+                      <a href="<?php echo $event['event_url']; ?>" target="_blank"><?php echo $event['title']; ?></a>
+                    </h3>
+                    <?php
+                    $date_from = date('jS M Y', strtotime($event['date_from']));
+                    $date_to = $event['date_to'] ? date('jS M Y', strtotime($event['date_to'])) : null;
+                    $time_from = date('H:i', strtotime($event['date_from'])) ?>
+
+                    <div class="xinc-events__event_list__event_tile__details__date">
+                      <div class="xinc-events__event_list__event_tile__details__item">
+                        <?php echo get_template_part('src/xinc-events/svg/date'); ?>
+                        <?php if($date_to && ($date_from !== $date_to)) { ?>
+                          <?php echo $date_from; ?> - <?php echo $date_to; ?>
+                        <?php } else { ?>
+                          <?php echo $date_from; ?>
+                        <?php } ?>
+                      </div>
+                      <div class="xinc-events__event_list__event_tile__details__item">
+                        <?php echo get_template_part('src/xinc-events/svg/time'); ?>Begins at <?php echo $time_from; ?>
+                      </div>
+                    </div>
+                    <?php if($event['location']) { ?>
+                      <div class="xinc-events__event_list__event_tile__details__item">
+                        <?php echo get_template_part('src/xinc-events/svg/location'); ?><?php echo xinc_events_link_urls($event['location']); ?>
+                      </div>
+                    <?php } ?>
+
+                    <div>
+                      <?php $extract = xinc_events_first_words($event['description'], 20) ?>
+                      <?php echo $extract; ?>&hellip;
+                    </div>
+
+                    <div>
+                      <a class="btn btn-primary" href="<?php echo $event['event_url']; ?>" target="_blank">Buy Tickets & More Info</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          <?php } ?>
+        </div>
+      <?php } ?>
+
+<?php endwhile; ?>
   </div>
 </main>
