c07be4a30a2d8ad5b10a53a2c0bc846683d9eaad
Beef <mark@benewith.com>
2023-11-14 13:09:08 +0000

    add iframe capability to events list

--- template-events.php
+++ template-events.php
@@ -3,9 +3,8 @@
  * Template Name: Xinc Events Iframe Page
  */
 
-
-get_header(); ?>
+get_header('bare'); ?>
 
 <?php get_template_part('templates/partials/events/list-events'); ?>
 
-<?php get_footer();
+<?php get_footer('bare');
