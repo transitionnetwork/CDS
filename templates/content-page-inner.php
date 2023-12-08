8fc4bd597a87741492ff5d2c5b1df594abf1da23
Beef <mark@benewith.com>
2023-12-08 14:08:48 +0000

    remove debug code

--- templates/content-page-inner.php
+++ templates/content-page-inner.php
@@ -3,7 +3,6 @@
     <?php while (have_posts()) : the_post(); ?>
       <h1><?php echo \Tofino\Helpers\title(); ?></h1>
       <?php $events = xinc_events_get_events('https://events.transitionmovement.org/inner/'); ?>
-      <?php var_dump($events); ?>
       <?php get_template_part('templates/partials/events/table-events', null, array('events' => $events)); ?>
     <?php endwhile; ?>
   </div>
