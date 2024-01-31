14045d19b0eb9d0e5dc685373ba7d8df0829e6f0
Beef <mark@benewith.com>
2024-01-31 15:42:36 +0000

    allow better frontend trainer admin

--- templates/buttons/publish-delete.php
+++ templates/buttons/publish-delete.php
@@ -4,7 +4,7 @@
       <?php echo svg('check'); ?><?php _e('Publish', 'tofino'); ?>
     </button>
   
-    <?php $confirm_message = __('Are you sure you want to delete this group? This is permanent!', 'tofino'); ?>
+    <?php $confirm_message = __('Are you sure you want to delete this group?', 'tofino'); ?>
     <button name="trash_group_id" value="<?php echo ($args['post_id']); ?>" class="btn btn-danger btn-sm" onclick="return confirm('<?php echo $confirm_message; ?>')">
       <?php echo svg('trashcan'); ?><?php _e('Delete', 'tofino'); ?>
     </button>
--- templates/partials/form-toggle-trainer-state.php
+++ templates/partials/form-toggle-trainer-state.php
@@ -1,10 +1,20 @@
 <?php if(is_user_trainer_admin()) { ?>
   <form action="" method="POST" class="mt-3">
     <input type="hidden" name="post_id" value="<?php echo get_the_ID(); ?>">
+
+     <?php $confirm_message = __('Are you sure you want to delete this trainer?', 'tofino'); ?>
+
     <?php if(get_post_status() === 'pending' && is_user_trainer_admin()) { ?>
-      <button class="btn btn-success btn-sm" name="trainer_update" value="publish">Publish Trainer</button>
+      <button class="btn btn-success btn-sm" name="trainer_update" value="publish"><?php echo svg('check'); ?>Publish Trainer</button>
+      <button class="btn btn-danger btn-sm" name="trainer_update" value="trash" onclick="return confirm('<?php echo $confirm_message; ?>')"><?php echo svg('trashcan'); ?>Delete Trainer</button>
     <?php } else { ?>
-      <button class="btn btn-danger btn-sm" name="trainer_update" value="pending">Unpublish Trainer</button>
+      <button class="btn btn-danger btn-sm" name="trainer_update" value="pending"><?php echo svg('x'); ?>Unpublish Trainer</button>
+    <?php } ?>
+
+    <?php if(is_user_trainer_admin()) { ?>
+      <div class="trainer-created">
+        <?php _e('Created: '); ?><?php echo get_the_date(); ?>
+      </div>
     <?php } ?>
   </form>
 <?php } ?>
