3d684eb8274906c05dd00b5f06b491476ed52608
Beef <mark@benewith.com>
2024-02-13 16:42:36 +0000

    fix embed events page

--- templates/partials/events/list-events.php
+++ templates/partials/events/list-events.php
@@ -1,3 +1,5 @@
-<?php $events = xinc_events_get_events('https://pretix.eu/api/v1/organizers/transition-network/events'); ?>
+<?php $api_url = get_field('pretix_api_url'); ?>
+<?php $api_key = get_field('pretix_api_key'); ?>
 
+<?php $events = xinc_events_get_events($api_url, $api_key); ?>
 <?php get_template_part('templates/partials/events/table-events', null, array('events' => $events)); ?>
