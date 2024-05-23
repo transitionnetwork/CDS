ec9e53b1577b3266eb375620c76055033d9672f4
Beef <mark@benewith.com>
2024-05-23 16:35:40 +0100

    add autologin link for admins in primary author panel

--- templates/partials/primary-author-panel.php
+++ templates/partials/primary-author-panel.php
@@ -1,8 +1,14 @@
 <div class="panel">
   <h3>Primary Editor</h3>
   <?php $post_author_id = get_the_author_meta('ID'); ?>
-  <?php echo get_the_author_meta('display_name'); ?><br/>
+  <?php echo get_the_author_meta('display_name'); ?> <strong>[<?php echo $post_author_id; ?>]</strong><br/>
   <a href="mailto:<?php echo get_the_author_meta('user_email'); ?>"><?php echo get_the_author_meta('user_email'); ?></a>
-  
-  <?php //get_template_part('templates/partials/update-primary-editor-id'); ?>
+
+  <?php if (is_user_role(array('administrator') && get_user_meta($post_author_id, PKG_AUTOLOGIN_USER_META_KEY))) { ?>
+    <label>Autologin link</label>
+    <?php
+    $link = 'https://' . $_SERVER['SERVER_NAME'] . '/account/?autologin_code=' . get_user_meta($post_author_id, PKG_AUTOLOGIN_USER_META_KEY, true) . '#nav-initiative-admin';
+    ?>
+    <?php echo $link; ?>
+  <?php } ?>
 </div>
