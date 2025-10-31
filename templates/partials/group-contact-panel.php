52fe7f49a7c8b25fe98d56c6f7256e5cb4085b17
Beef <mark@benewith.com>
2025-10-31 14:42:52 +0000

    add reg number

--- templates/partials/group-contact-panel.php
+++ templates/partials/group-contact-panel.php
@@ -9,6 +9,8 @@ $link_fields = array(
   'youtube',
 );
 
+$reg_number = get_field('group_detail_registration_number');
+
 $has_links = false;
 
 foreach($link_fields as $field) {
@@ -17,38 +19,43 @@ foreach($link_fields as $field) {
   }
 } ?>
 
-<?php if($email || $has_links || $additional) { ?>
-  <div class="panel group-contact">
-    <?php if ($email) { ?>
-      <div class="mb-3">
-        <h3>Email</h3>
-        <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
-      </div>
-    <?php } ?>
+<div class="panel group-contact">
+  <?php if ($email) { ?>
+    <div class="mb-3">
+      <h3>Email</h3>
+      <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
+    </div>
+  <?php } ?>
+
+  <?php if($has_links) { ?>
+    <div>
+      <h3><?php _e('Links', 'tofino'); ?></h3>
+      
+      <ul class="links">
+        <?php foreach($link_fields as $field) {
+          if(get_field($field)) { ?>
+            <li><a href="<?php echo get_field($field); ?>" target="_blank"><?php echo svg($field); ?></a></li>
+          <?php } ?>
+        <?php } ?>
+      </ul>
+    </div>
+  <?php } ?>
   
-    <?php if($has_links) { ?>
-      <div>
-        <h3><?php _e('Links', 'tofino'); ?></h3>
-        
-        <ul class="links">
-          <?php foreach($link_fields as $field) {
-            if(get_field($field)) { ?>
-              <li><a href="<?php echo get_field($field); ?>" target="_blank"><?php echo svg($field); ?></a></li>
-            <?php } ?>
-          <?php } ?>
-        </ul>
-      </div>
-    <?php } ?>
-    
-    <?php if($additional) { ?>
-      <div>
-        <h3 class="mt-3"><?php _e('More Links', 'tofino'); ?></h3>
-        <ul>
-          <?php foreach($additional as $item) { ?>
-            <li><a href="<?php echo $item['address']; ?>" target="_blank"><?php echo $item['label']; ?></a></li>
-          <?php } ?>
-        </ul>
-      </div>
-    <?php } ?>
-  </div>
-<?php } ?>
+  <?php if($additional) { ?>
+    <div class="mt-3">
+      <h3><?php _e('More Links', 'tofino'); ?></h3>
+      <ul>
+        <?php foreach($additional as $item) { ?>
+          <li><a href="<?php echo $item['address']; ?>" target="_blank"><?php echo $item['label']; ?></a></li>
+        <?php } ?>
+      </ul>
+    </div>
+  <?php } ?>
+
+  <?php if($reg_number) { ?>
+    <div class="mt-3">
+      <h3><?php _e('Registration Number', 'tofino'); ?></h3>
+      <div><?php echo $reg_number; ?></div>
+    </div>
+  <?php } ?>
+</div>
