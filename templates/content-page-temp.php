269f370d99b1ed1fc279b63e5e7b5354819e894e
Beef <mark@benewith.com>
2025-11-18 14:33:29 +0000

    add country taxonomy emptying function

--- templates/content-page-temp.php
+++ templates/content-page-temp.php
@@ -1,50 +1,16 @@
 <?php
-function clean_dev_log_entries() {
+function remove_country_terms_from_trainer_posts() {
   $args = array(
-    'post_type' => 'initiatives',
-    'meta_query' => array(
-      array(
-        'key' => 'mail_log',
-        'compare' => 'EXISTS'
-      )
-    ),
+    'post_type' => 'trainers',
     'posts_per_page' => -1
   );
 
   $posts = get_posts($args);
-  if($posts) {
-    // foreach($posts as $post) {
-    //   var_dump(get_post_meta($post->ID, 'mail_log', true));
-    // }
-    // die();
-    
-    foreach($posts as $post) {
-      $last_event = get_post_meta( $post->ID, 'last_mail_event' );
-      $log = get_post_meta($post->ID, 'mail_log', true);
-  
-      var_dump($log);
-    
-    
-      foreach($log as $key => $item) {
-        if(str_contains($item, 'benewith') || str_contains($item, 'xinc')) {
-          unset($log[$key]);
-        }
-      }
-  
-      $mail_log = array_values($log);
-      // update_post_meta( $post->ID, 'mail_log', $mail_log);
-  
-      $last_key = end(array_keys($log));
-      
-      $last_time = substr($log[$last_key], 0, 19);
-      update_post_meta( $post->ID, 'last_mail_date', $last_time);
-    
-      $split = explode(" ", $log[$last_key]);
-      $last_word = $split[count($split)-1];
-      update_post_meta( $post->ID, 'last_mail_event', $last_word);
-    }
 
-    var_dump('complete');
+  foreach($posts as $post) {
+    wp_set_post_terms($post->ID, array(), 'country');
+    var_dump('Removed country terms from post ID: ' . $post->ID);
   }
-  
 }
+
+remove_country_terms_from_trainer_posts();
