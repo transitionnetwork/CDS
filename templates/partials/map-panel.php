c85dc03dbfc45f308360c88539f8240f709f59e3
Beef <mark@benewith.com>
2025-12-14 17:49:58 +0000

    add new logo to map embed

--- templates/partials/map-panel.php
+++ templates/partials/map-panel.php
@@ -1,7 +1,7 @@
 <div id="map-info">
   <a href="<?php echo home_url(); ?>" target="_parent">
     <span class="sr-only">Transiton Network website</span>
-    <?php echo svg(array('sprite' => 'tn-logo', 'class' => 'logo')); ?>
+    <?php echo svg(array('sprite' => 'tn-network-25', 'class' => 'logo')); ?>
   </a>
   <div id="map-info-panel">
     <button class="close" aria-label="close panel">
