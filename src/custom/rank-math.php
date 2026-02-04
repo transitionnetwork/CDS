47580576d1ab3410011d7e2e41a15aaa39ddad0d
Beef <mark@benewith.com>
2026-02-04 15:43:58 +0000

    fixed bug affecting certain groups

--- src/custom/rank-math.php
+++ src/custom/rank-math.php
@@ -18,7 +18,7 @@ add_action( 'rank_math/head', function() {
   if(is_singular('initiatives') || is_singular('trainers')) {
     add_filter( "rank_math/opengraph/facebook/og_image", function($data) {
       $image_field = get_custom_og_image();
-      if($image_field) {
+      if(is_array($image_field)) {
         return $image_field['url'];
       } else {
         return $data;
@@ -27,7 +27,7 @@ add_action( 'rank_math/head', function() {
     
     add_filter( "rank_math/opengraph/facebook/og_image_secure_url", function($data) {
       $image_field = get_custom_og_image();
-      if($image_field) {
+      if(is_array($image_field)) {
         return $image_field['url'];
       } else {
         return $data;
@@ -36,7 +36,7 @@ add_action( 'rank_math/head', function() {
 
     add_filter( "rank_math/opengraph/facebook/og_image_width", function($data) {
       $image_field = get_custom_og_image();
-      if($image_field) {
+      if(is_array($image_field)) {
         return $image_field['width'];
       } else {
         return $data;
@@ -45,7 +45,7 @@ add_action( 'rank_math/head', function() {
     
     add_filter( "rank_math/opengraph/facebook/og_image_height", function($data) {
       $image_field = get_custom_og_image();
-      if($image_field) {
+      if(is_array($image_field)) {
         return $image_field['height'];
       } else {
         return $data;
@@ -54,7 +54,7 @@ add_action( 'rank_math/head', function() {
     
     add_filter( "rank_math/opengraph/facebook/og_image_type", function($data) {
       $image_field = get_custom_og_image();
-      if($image_field) {
+      if(is_array($image_field)) {
         return $image_field['mime_type'];
       } else {
         return $data;
@@ -63,7 +63,7 @@ add_action( 'rank_math/head', function() {
     
     add_filter( "rank_math/opengraph/twitter/twitter_image", function($data) {
       $image_field = get_custom_og_image();
-      if($image_field) {
+      if(is_array($image_field)) {
         return $image_field['url'];
       } else {
         return $data;
