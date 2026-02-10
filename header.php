80acc7c3aa5cf088ece4fdf7986bcf76aada563f
Beef <mark@benewith.com>
2026-02-10 12:57:55 +0000

    add gtag

--- header.php
+++ header.php
@@ -17,6 +17,16 @@ use \Tofino\ThemeOptions\Notifications as n; ?>
 
   <script src="https://analytics.ahrefs.com/analytics.js" data-key="b+sXmeHi26Fyp3pKXV0AFQ" async></script>
 
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-934395512"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-934395512');
+  </script>
+
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
