269646cd2e14b9a56b1739f45817893747eb3091
Beef <mark@benewith.com>
2026-03-05 15:05:11 +0000

    redirect not logged in user to login form

--- templates/content-page-identify.php
+++ templates/content-page-identify.php
@@ -1,6 +1,6 @@
 <?php if (!is_user_logged_in()) { ?>
   <?php
-    wp_redirect(esc_url(add_query_arg('error_code', '1', '/error')));
+    wp_redirect(esc_url(add_query_arg('error_code', '1', '/member-login')));
     exit;
   ?>
 <?php } else { ?>
